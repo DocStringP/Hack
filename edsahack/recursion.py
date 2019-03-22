1c9d3ab68bfa891cdb8cf8790e8e1de8a58c4b91
Sylvester Manganye <manganye.so@gmail.com>
2019-03-22 14:27:27 +0200

    update

--- edsahack/recursion.py
+++ edsahack/recursion.py
@@ -20,7 +20,7 @@ def fibonacci(n):
     '''Return nth term in fibonacci sequence'''
 
     if n <= 1:
-        return 1
+        return 0
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
@@ -31,7 +31,7 @@ def factorial(n):
     '''Return n!'''
 
     if n <= 1:
-        return n
+        return 1
     else:
         return n * factorial(n-1)
 
