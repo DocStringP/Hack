3f399e2ee2d46c0a0f098459738d9cc4786e6ceb
Lesa Moloi <lesamoloi11@gmail.com>
2019-03-22 15:03:56 +0200

    First Commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='edsahack',
+    name='Hack',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/VestaM94/edsahack.git',
-    author='<Sylvester Manganye>',
-    author_email='<manganye.so@gmail.com>'
+    url='https://github.com/DocStringP/Hack.git',
+    author='Lesa Moloi>',
+    author_email='<lesamoloi11@gmail.com>'
 )
